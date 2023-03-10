357769ba7e18633de72c7e613f8972b572ce618c
Jardel de Castro Queiroz <jardeljcadm@gmail.com>
2023-03-09 22:51:33 -0300

    Site finalizado
    Graças a Deus

--- Principal/JS.js
+++ Principal/JS.js
@@ -1,9 +1,9 @@
 /* Função sortear números */
-function SortearNumeros(){
-    var numeros [];
+function SortearNumeros() {
+    var numeros = [];
 
 /* O código abaixo irá gerar 6 nuúmeros aleatórios */
-    while(numeros.leagth < 6){
+    while(numeros.length < 6) {
         var num = Math.floor(Math.random()*61);
         if(!numeros.includes(num)){
             numeros.push(num);
@@ -11,10 +11,10 @@ function SortearNumeros(){
     }
 
 /* O trecho de código abaixo exibirá os números sorteados na página */
-    document.getElementById("numero1").innerHTML = numeros[0];
-    document.getElementById("numero2").innerHTML = numeros[1];
-    document.getElementById("numero3").innerHTML = numeros[2];
-    document.getElementById("numero4").innerHTML = numeros[3];
-    document.getElementById("numero5").innerHTML = numeros[4];
-    document.getElementById("numero6").innerHTML = numeros[5];
+    document.getElementById("num1").innerHTML = numeros[0];
+    document.getElementById("num2").innerHTML = numeros[1];
+    document.getElementById("num3").innerHTML = numeros[2];
+    document.getElementById("num4").innerHTML = numeros[3];
+    document.getElementById("num5").innerHTML = numeros[4];
+    document.getElementById("num6").innerHTML = numeros[5];
 }
